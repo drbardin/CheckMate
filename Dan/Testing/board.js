9442953cd1ba0b1c89285b61462a099fa5c659af
drbardin <drbardin@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-11-20 16:13:09 +0000

    Working files after demo 3

--- Dan/Testing/board.js
+++ Dan/Testing/board.js
@@ -1,58 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var canvas = $("canvas#gameCanvas")[0];
     var ctx = canvas.getContext("2d");
     var w = $("canvas#gameCanvas").width();
     var h = $("canvas#gameCanvas").height();
-    
-    // Image value for clipping
+
+    // value id for piece location in pieces.png. 
+    // used for cropping method. 
     var PAWN = 0;
     var KNIGHT = 1;
     var BISHOP = 2;
     var ROOK = 3;
     var QUEEN = 4;
     var KING = 5;
-    
-    var UNCAPTURED;
-    var CAPTURED;
-    var WHITE_TURN; 
-    var NUM_ROWS;
-    var SQR_SIZE = h / 8;
-    var IGNORE_CLICKS; 
-    var PIECE_HAS_HIGHLIGHT; 
-    var LAST_IMAGE_CLICKED = {}; 
-    var player_piece_selection = {row: null, col: null, arr_pos: null, im_type: null};
-    var prev_piece_selection = {row: null, col: null, arr_pos: null, im_type: null};
 
-    // var pieceIDs = { bRook, bKnight, bBishop, bKing, bQueen, bPawn,
-    //                  wRook, wKnight, wBishop, wKing, wQueen, wPawn }; 
-    var click_count = 0; 
-    var pieces = null; 
-    var json = null; 
-    var sqr_clicked = { 'row': null, 'col': null, 'arr_pos': null };
-    
+    var NUM_ROWS;
+    var CAPTURED;
+    var UNCAPTURED;
+    var SQR_SIZE = h / 8;
+    var WHITE_TURN;
+
+    var click_counter = 0; 
+    var prev_click = {row: null, col: null};
+    var sqrs_with_highlight = new Array();   // array of (r,c) objects currently highlighted. 
+    var before_highlight_imgs = new Array(); // array of image object before highlighting. 
+
+    // object declarations
+    var pieces = null;
+    var json = null;
+    var JSONObj = null;
+
     // This acts as main method
     function draw() {
-        
         // white always makes first move.
         WHITE_TURN = true;
-
+        // load chess piece images, draw them.
         pieces = new Image();
         pieces.src = 'pieces.png';
         pieces.onload = drawPieces;
 
         // call method to set up fresh chessboard
         setNewBoard();
-
-        //canvas.addEventListener('click', heard_click, false);
     }
     draw();
-    
+
     // JSON wrapped in function so can be used as a new game initializer.
-    function setNewBoard() {
+   function setNewBoard() {
         // two arrays, one black, one white. Their positions are 0-15. 
         json = {
-            'black': [ { 'piece': ROOK,  'row': 0, 'col': 0, 'status': UNCAPTURED },
+            "black": [ { 'piece': ROOK,  'row': 0, 'col': 0, 'status': UNCAPTURED },
                        { 'piece': KNIGHT,'row': 0, 'col': 1, 'status': UNCAPTURED },
                        { 'piece': BISHOP,'row': 0, 'col': 2, 'status': UNCAPTURED },
                        { 'piece': KING,  'row': 0, 'col': 3, 'status': UNCAPTURED },
@@ -69,7 +65,7 @@ $(document).ready(function() {
                        { 'piece': PAWN,  'row': 1, 'col': 6, 'status': UNCAPTURED },
                        { 'piece': PAWN,  'row': 1, 'col': 7, 'status': UNCAPTURED },
                      ],
-            'white': [ { 'piece': ROOK,  'row': 7, 'col': 0, 'status': UNCAPTURED },
+            "white": [ { 'piece': ROOK,  'row': 7, 'col': 0, 'status': UNCAPTURED },
                        { 'piece': KNIGHT,'row': 7, 'col': 1, 'status': UNCAPTURED },
                        { 'piece': BISHOP,'row': 7, 'col': 2, 'status': UNCAPTURED },
                        { 'piece': KING,  'row': 7, 'col': 3, 'status': UNCAPTURED },
@@ -91,146 +87,209 @@ $(document).ready(function() {
     ///////////////////////////////
     //   DRAW PIECES FUNCTIONS   //
     ///////////////////////////////
-    function drawPieces() 
-    {
+    function drawPieces() {
         //Send white team to be drawn, then black team. 
         drawAllPieces(json.white, false);
         drawAllPieces(json.black, true);
     }
-
     // This send all pieces to be drawn in their base position.
-    function drawAllPieces(fullColorSet, isBlackTeam) 
-    { 
+    function drawAllPieces(fullColorSet, isBlackTeam) {
         var piece_index;
         // go through black or white team one-by-one, send to drawImage function 
         for (piece_index = 0; piece_index < fullColorSet.length; piece_index++) {
-            var curImage = fullColorSet[piece_index]; 
+            var curImage = fullColorSet[piece_index];
             drawThisPiece(curImage, isBlackTeam);
         }
     }
     // Draws the specified piece. 
-    function drawThisPiece(pieceToDraw, isBlackTeam)
-    {
+    function drawThisPiece(pieceToDraw, isBlackTeam) {
         var thisXY = getCoordinates(pieceToDraw.piece, isBlackTeam);
         ctx.drawImage(pieces, thisXY.x, thisXY.y, SQR_SIZE, SQR_SIZE, pieceToDraw.col * SQR_SIZE, pieceToDraw.row * SQR_SIZE, SQR_SIZE, SQR_SIZE);
     }
     // this chops out a 100x100 square from the pieces.png. Hopefully it'll be the right color and rank. 
-    function getCoordinates(thisPiece, isBlackTeam)
-    {
+    function getCoordinates(thisPiece, isBlackTeam) {
         // x is the column, so send the number of the current piece (e.g., ROOK = 3), multiply by size of piece images.
         // y is row, or white/black team. 
-        var im_coordinates = { "x": thisPiece * SQR_SIZE, "y": (isBlackTeam ? SQR_SIZE : 0)};
+        var im_coordinates = {
+            "x": thisPiece * SQR_SIZE,
+            "y": (isBlackTeam ? SQR_SIZE : 0)
+        };
         return im_coordinates;
     }
-    
-    // run through piece array, check for match. 
-    function getPieceClicked(sqr_clicked) {
-        
-        var cur_color_array = {};
-        var arr_pos, im_type, row, col; 
-        
-        // which color array should be looped?
-        if(WHITE_TURN? cur_color_array = json.white: cur_color_array = json.black);
-        
-        // loop the array, if we have a match, build the data. 
-        for(var i = 0; i < cur_color_array.length; i++)
-        {
-            if(cur_color_array[i].row === sqr_clicked.row && cur_color_array[i].col === sqr_clicked.col)
-            {
-                arr_pos = i;
-                im_type = cur_color_array[i].piece;
-                row = cur_color_array[i].row;
-                col = cur_color_array[i].col;
+
+    function highlight_squares(sqrs_to_highlight) {
+        for (var i = 0; i < sqrs_to_highlight.length; i++) {
+            var r = sqrs_to_highlight[i].row;
+            var c = sqrs_to_highlight[i].col;
+
+            // add a row and col to array holding highlight coords.
+            sqrs_with_highlight.push(new Object());
+            sqrs_with_highlight[i].row = r;
+            sqrs_with_highlight[i].col = c;
+
+            // add un-highlighted image to array. 
+            before_highlight_imgs[i] = ctx.getImageData(c * 100, r * 100, 100, 100);
+
+            // draw highlight box around clicked square in red, potential moves in green
+            ctx.outlineWidth = 5;
+            if (i === 0) // clicked box
+                ctx.strokeStyle = "#FF0000"; // red
+            else
+                ctx.strokeStyle = "#66FF00"; // green
+            ctx.strokeRect((c * 100) + 3, (r * 100) + 3, 100 - (3 * 2), 100 - (3 * 2));
+        }
+    }
+
+    function remove_highlights() {
+        for (var i = 0; i < sqrs_with_highlight.length; i++) {
+            var r = sqrs_with_highlight[i].row;
+            var c = sqrs_with_highlight[i].col;
+
+            ctx.putImageData(before_highlight_imgs[i], c * 100, r * 100);
+        }
+    }
+
+    function update_json(move_to_click) {
+        var cur_color;
+        var r = move_to_click.row;
+        var c = move_to_click.col;
+        if (WHITE_TURN ? cur_color = json.white : cur_color = json.black);
+
+        for (var i = 0; i < cur_color.length; i++) {
+            if (cur_color[i].row === prev_click.row && cur_color[i].col === prev_click.col) {
+                // update the json to reflect new position.
+                cur_color[i].row = r;
+                cur_color[i].col = c;
             }
         }
-                
-        return {
-            arr_pos: arr_pos,
-            im_type: im_type,
-            row: row,
-            col: col
+    }
+
+    // Click handler 
+    // Listen for button click
+    $("canvas#gameCanvas").click(function (e) {
+
+        // increment click counter
+        click_counter++;
+
+        // Get (x,y) of mouse click ( pageX works best in this situation. )
+        // NOTE: (0,0) is top-left corner of canvas. 
+        var x = e.pageX - $("canvas#gameCanvas").offset().left;
+        var y = e.pageY - $("canvas#gameCanvas").offset().top;
+
+        //  Find out which square on the canvas was clicked.
+        //  row = Y val / 100 (size of a square) and then rounded down. This will give a num 0-7
+        //  col = X val / 100 and then rounded down. This will also give a num 0-7
+        var row_clicked = Math.floor(y / SQR_SIZE);
+        var col_clicked = Math.floor(x / SQR_SIZE);
+
+
+        // JSON for outgoing click
+        var JSONObj = {
+            "row": row_clicked,
+            "col": col_clicked,
+            "black": json.black,
+            "white": json.white,
         };
-    }
-        
-// Listen for button click
-$("canvas#gameCanvas").click(function(e) {
-    
-    console.log("hello click");
-    
-    // Get (x,y) of mouse click ( pageX works best in this situation. )
-    // NOTE: (0,0) is top-left corner of canvas. 
-    var x = e.pageX-$("canvas#gameCanvas").offset().left;
-    var y = e.pageY-$("canvas#gameCanvas").offset().top;
 
-    //Find out which square on the canvas was clicked.
-    //   row = Y val / 100 (size of a square) and then rounded down. This will give a num 0-7
-    //   col = X val / 100 and then rounded down. This will also give a num 0-7
-    var row_clicked = Math.floor(y / SQR_SIZE);
-    var col_clicked = Math.floor(x / SQR_SIZE);
-    
-    var cur_click = {row: row_clicked, col: col_clicked};
-    click_count++;
-    
-    console.log("clk count: " + click_count);
-    console.log("cur click - row:" + cur_click.row);
-    console.log("cur click - col:" + cur_click.col);
+        // Turns JSONObj into a JSONStr. 
+        var JSONStr = JSON.stringify(JSONObj);
+        console.log(JSONStr);
 
-    if(PIECE_HAS_HIGHLIGHT !== true)
-    {
-        // grab un-highlighted image
-        LAST_IMAGE_CLICKED = ctx.getImageData(col_clicked*100, row_clicked*100, 100, 100);
+        // FROM_CLICK condition
+        if (click_counter % 2 !== 0) {
 
-        //draw highlight box around user selection
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "#66FF00";
-        ctx.strokeRect((col_clicked * SQR_SIZE) + 3, (row_clicked * SQR_SIZE) + 3, SQR_SIZE - (3 * 2), SQR_SIZE - (3 * 2));
-        PIECE_HAS_HIGHLIGHT = true;
+            $.ajax({
+                type: 'POST',
+                url: 'from_click.php',
+                // If I try to pass the JSON to 'data' in a different way, I get null json               
+                //properties echoed back.
+                data: {
+                    'data': JSONStr
+                },
 
-        player_piece_selection = getPieceClicked(cur_click);
-        console.log("after setting player piece selection r/c: " + player_piece_selection.row + " " + player_piece_selection.col);
-    }
-    else
-    {
-        //prev_piece_selection.col = player_piece_selection.col;
-        //prev_piece_selection.row = player_piece_selection.row; 
-        prev_piece_selection = player_piece_selection;
-        console.log("prev.row: " + prev_piece_selection.row + "prev.col: " + prev_piece_selection.col);
-        
-        player_piece_selection = getPieceClicked(cur_click);
-        console.log("pps.row: " + player_piece_selection.row + "pps.col: " + player_piece_selection.col);
-        
-        ctx.clearRect(prev_piece_selection.col*100 , prev_piece_selection.row*100 , 100 , 100 );
-        console.log("last image clicked: " + LAST_IMAGE_CLICKED);
-        ctx.putImageData(LAST_IMAGE_CLICKED, prev_piece_selection.col*100, prev_piece_selection.row*100);
-        PIECE_HAS_HIGHLIGHT = false;
-        
-        // move the piece
-        ctx.clearRect(prev_piece_selection.col*100 , prev_piece_selection.row*100 , 100 , 100 );
-        ctx.putImageData(LAST_IMAGE_CLICKED, col_clicked*100, row_clicked*100);
-        
-    }
-    
-    console.log("goodbye click");
+                // If ContentType is lower-case, causes null json properties to be echoed back.
+                ContentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (data, textStatus, jqXHR) {
+                    console.log(textStatus);
+                    console.log(data);
+                    console.log(JSON.stringify(data));
+                    console.log("Heard reply from from_click.php");
+
+                    // call process return data function
+
+                    highlight_squares(data);
+                    prev_click.col = col_clicked;
+                    prev_click.row = row_clicked;
+                },
+                error: function (xhr, desc, err) {
+                    console.log("No reply from from_click.php");
+                }
+            });
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: 'to_click.php',
+                // If I try to pass the JSON to 'data' in a different way, I get null json               
+                //properties echoed back.
+                data: {
+                    'data': JSONStr
+                },
+
+                // If ContentType is lower-case, causes null json properties to be echoed back.
+                ContentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (data, textStatus, jqXHR) {
+                    console.log(textStatus);
+                    console.log(data);
+                    console.log(JSON.stringify(data));
+                    console.log("Heard reply from to_click.php");
+
+                    // undo highlighted squares
+                    remove_highlights();
+
+                    // move piece
+                    ctx.clearRect(prev_click.col * 100, prev_click.row * 100, 100, 100);
+                    ctx.putImageData(before_highlight_imgs[0], col_clicked * 100, row_clicked * 100);
+
+                    sqrs_with_highlight = new Array(); // clear this turns array
+                    before_highlight_imgs = new Array(); // clear 
+                    prev_click = {
+                        "row": null,
+                        "col": null
+                    };
+
+                    if (WHITE_TURN ? WHITE_TURN = false : WHITE_TURN = true);
+                },
+                error: function (xhr, desc, err) {
+                    console.log("No reply from to_click.php");
+                }
+            });
+        }
     });
-});
+    // getUpdate();
+    //    function getUpdate() {
+    //        var update_ping = $.ajax( {
+    //           url: "loader.php",
+    //           type: 'GET',
+    //           dataType: "json"
+    //        });
+    //        
+    //        update_ping.done(function(data, textStatus, jqXHR) {
+    //            $.parseJSON(data);
+    //            
+    //            var lrow = data[0].row;
+    //            var lcol = data[0].col;
+    //            console.log("update jquery row: " + lrow);
+    //            console.log("update jquery col: " + lcol);
+    //            // send object to draw function
+    //        });
+    //        
+    //        update_ping.fail(function(jqXHR, textStatus) {
+    //           alert("Ping failed: " + textStatus);
+    //        });
+    //    }
 
-
-//    function highlightSquare(player_piece_selection)
-//    {
-//        var row_clicked = player_piece_selection.row;
-//        var col_clicked = player_piece_selection.col;
-//        
-//        //Take a snapshot of clicked piece before highlighting.
-//        var cur_click_image = ctx.getImageData(col_clicked*100, row_clicked*100, 100, 100);
-//
-//        //draw highlight box around user selection
-//        ctx.lineWidth = 5;
-//        ctx.strokeStyle = "#66FF00";
-//        ctx.strokeRect((col_clicked * SQR_SIZE) + 3, (row_clicked * SQR_SIZE) + 3, SQR_SIZE - (3 * 2), SQR_SIZE - (3 * 2));        
-//
-//    }
-//        function undoHighlight(player_piece_selection)
-//    {
-//        ctx.clearRect(player_piece_selection.col*100, player_piece_selection.row*100, 100, 100 );
-//        ctx.putImageData(player_piece_selection.col, player_piece_selection.row);
-//    }
+ });
+    
+                                
---
+++ Dan/Testing/classLobby.php
@@ -0,0 +1,328 @@
+<?php
+//error_reporting(E_ALL);
+//ini_set('display_errors', 'On');
+include_once "classPlayer.php";
+session_start();
+?>
+
+<?php
+
+    class Lobby
+    {
+        private $player_id;
+        private $time_entered;
+    
+        /* 
+            Default Constructor:
+                Sets variable values based upon SESSION variables and PHP's date() function. 
+        */
+/*        public function __construct()
+        {
+            $this->player_id = $_SESSION["player"]->get_Id();
+            // $this->time_entered = date('Y-m-d G:i:s');
+        }*/
+        
+        function __construct()
+        {
+            $args = func_get_args();
+            $num_args = func_num_args();
+            if (method_exists($this, $f='__construct'.$num_args))
+            {
+                call_user_func_array(array($this,$f),$args);   
+            }
+            else
+            {
+                $this->player_id = $_SESSION["player"]->get_Id();
+            }
+            
+        }
+        
+        /*
+            Overloaded Constructor:
+                Sets variable values based upon parameter input.
+            
+            NOTE: ERROR-CHECKING NOT YET IMPLEMENTED
+        */
+        public function __construct2($id, $time)
+        {
+            $this->player_id = $id;
+            $this->time_entered = $time;
+        }
+        
+        
+        
+        /*
+            Getter for player_id:
+                Returns int player_id
+        */
+        public function get_Player_Id()
+        {
+             if (isset($this->player_id))
+            {
+                  return $this->player_id;
+            }
+            else 
+            {
+                echo "this->player_id not set.";
+            }           
+        }
+        
+        /*
+            Getter for time_entered:
+                Returns timestamp time_entered
+        */
+        public function get_Time_Entered()
+        {
+             if (isset($this->time_entered))
+            {
+                  return $this->time_entered;
+            }
+            else 
+            {
+                echo "this->time_entered not set.";
+            }           
+        }
+        
+        
+        
+        /*
+            Get oldest row entry from Lobby table:
+                Returns -1 if Lobby table is empty.
+                Returns instance of a Lobby class object containing the oldest row contents from the Lobby table.
+        */
+        public function get_Oldest_Entry()
+        {
+             $db_host = 'mysql.cs.iastate.edu';
+            $db_user = 'u309M13';
+            $db_pass = 'T2GWRYDIw';
+            $db_name = 'db309M13';           
+            $tbl_name = 'Lobby';
+            
+            // Create connection
+            $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+            
+            // Define SELECT statement.
+            $sql = "SELECT * FROM $tbl_name ORDER BY time_entered ASC LIMIT 1";
+            // Store result. 
+            $result = $conn->query($sql);
+            
+            // Check if Lobby table is empty.
+            // Otherwise, store row data and return an instance of the Lobby class. 
+            if ($result->num_rows < 1)
+            {
+                echo "get_Oldest_Entry(): Less than 1 row in result" . "<br/>";
+                $conn->close();
+                return -1;
+            }
+            else if ($result->num_rows === 1)
+            {
+                if ($row = $result->fetch_assoc())
+                {
+                    $lobby = new Lobby($row["player_id"],$row["time_entered"]);
+                    
+                    // Returns an instance of the Lobby class
+                    $conn->close();
+                    return $lobby;
+                }
+            }
+            else 
+            {
+                echo "Error: Too many rows returned" . "<br/>";
+            }
+            
+            // Close database connection. 
+            $conn->close();
+        }
+        
+        /*
+            Add current Lobby object contents to Lobby table.
+                No return value.
+        */
+        public function add_To_Table()
+        {
+            $db_host = 'mysql.cs.iastate.edu';
+            $db_user = 'u309M13';
+            $db_pass = 'T2GWRYDIw';
+            $db_name = 'db309M13';
+            $tbl_name = 'Lobby';
+            
+            // Create connection
+            $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+            
+            $pId = $this->player_id;
+            $timeEnt = $this->time_entered;
+            
+            // Define INSERT statement.
+            $sql = "INSERT INTO Lobby (player_id, time_entered) VALUES ('$pId','$timeEnt')";
+            
+            // Query the database with our statement. 
+            if ($conn->query($sql) === TRUE) 
+            {
+                echo "Player successfully added to Lobby." . "<br/>";
+            }
+            else
+            {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            
+            // Close database connection. 
+            $conn->close();
+        }
+          
+        
+        
+        /*
+            Deletes specified player from Lobby table.
+                No return value.
+        */
+        public function remove_Player($id)
+        {
+            $db_host = 'mysql.cs.iastate.edu';
+            $db_user = 'u309M13';
+            $db_pass = 'T2GWRYDIw';
+            $db_name = 'db309M13';           
+            $tbl_name = 'Lobby';
+            
+            // Create connection
+            $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+            
+            // Define DELETE statement.
+            $sql = "DELETE FROM $tbl_name WHERE player_id = $id";
+            
+            // Query the database with our statement. 
+            if ($conn->query($sql) === TRUE) 
+            {
+                echo "Player successfully removed from Lobby." . "<br/>";
+            }
+            else
+            {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            
+            // Close database connection. 
+            $conn->close();
+        }
+        
+        
+        
+        
+        
+        /*
+            Creates an entry in the Game table. Removes both players from the Lobby.
+                No return value.
+        */
+        public function create_Game($id_first, $id_second)
+        {
+            $db_host = 'mysql.cs.iastate.edu';
+            $db_user = 'u309M13';
+            $db_pass = 'T2GWRYDIw';
+            $db_name = 'db309M13';
+            $tbl_name = 'Game';
+            
+            //Parameter error checking
+            if ($id_first === $id_second)
+            {
+                // Throw error, id parameters must be different. 
+                echo "create_Game() Error: Paramters are identical. Must have two different account ids to create a game." . "<br/>";
+                return;
+            }
+            // Check for matching account on first id.
+            $white = get_Player($id_first);
+            if ($white === -1)
+            {
+                // Throw error, first parameter is not a valid account id.
+                echo "create_Game() Error: First parameter is not a valid account id." . "<br/>";
+                return;
+            }
+ 
+            // Check for matching account on second id. 
+            $black = get_Player($id_second);
+            if ($black === -1)
+            {
+                // Throw error, second parameter is not a valid account id.
+                echo "create_Game() Error: First parameter is not a valid account id." . "<br/>";
+                return;
+            }
+
+            // Create connection
+            $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+            
+            $wId = $white->get_Id();
+            $bId = $black->get_Id();
+            $wUsername = $white->get_Username();
+            $bUsername = $black->get_Username();
+            
+            // Define INSERT statement.
+            $sql = "INSERT INTO $tbl_name (white_id, black_id, turn_num, white_in, black_in, white_username, black_username) VALUES ('$wId', '$bId', 0, 0, 0,'$wUsername', '$bUsername')";
+            
+            // Query the database with our statement. 
+            if ($conn->query($sql) === TRUE) 
+            {
+                echo "Game successfully created." . "<br/>";
+            }
+            else
+            {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            
+            // Close database connection. 
+            $conn->close();
+            
+            // Remove both players from the lobby.
+            $this->remove_Player($id_first);
+            $this->remove_Player($id_second);
+        }
+        
+        /*
+            Attempts to add the player to the Lobby, but first checks if theres an person waiting in the lobby.
+            Will create a game if opponent is found. Otherwise, adds player to lobby.
+                No return value.
+        */
+        public function add_Player()
+        {
+            $db_host = 'mysql.cs.iastate.edu';
+            $db_user = 'u309M13';
+            $db_pass = 'T2GWRYDIw';
+            $db_name = 'db309M13';
+            $tbl_name = 'Game';
+
+            //NEED A CHECK HERE TO MAKE SURE THAT THE CURRENT ID ISN'T ALREADY IN A GAME.
+            //Create connection
+            $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+            // Check connection
+            if ($conn->connect_error) {
+               die("Connection failed: " . $conn->connect_error);
+            }
+
+            
+            $sql      = "SELECT * FROM $tbl_name WHERE '$this->player_id' = white_id OR '$this->player_id' = black_id";
+            $result   = $conn->query($sql);
+
+            // If result matched $username and $password, table row must be 1 row
+            if ($result->num_rows > 0)
+            {
+                echo "This player is already in a Game." . "<br/>";
+                $conn->close();
+                return;
+            }
+            else
+            {
+                $conn->close();
+                $opponent = $this->get_Oldest_Entry();
+    
+                if ($opponent === -1)
+                {
+                    echo "No opponent found." . "<br/>";
+                    $this->time_entered = date("Y-m-d H:i:s", time());
+                    $this->add_To_Table();
+                }
+                else
+                {
+                    echo "Opponent found. Create game." . "<br/>";
+                    $this->create_Game($opponent->get_Player_Id(),$this->player_id);
+                }
+            }
+        }
+    }
+?>1
