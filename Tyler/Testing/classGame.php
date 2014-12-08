002488c09115706cdb172b7f9187530c1f5e74ec
tjj <tjj@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-12-08 20:21:43 +0000

    All methods other than update_Game and remove_Game have been tested successfully.

--- Tyler/Testing/classGame.php
+++ Tyler/Testing/classGame.php
@@ -5,8 +5,7 @@ include_once "classPlayer.php";
 //include_once "engine.php";
 session_start();
 ?>
-<html>
-<body>
+
 <?php
 // game object definition
 class Game {
@@ -14,6 +13,8 @@ class Game {
 // Game Table Values
     // This player's game id.
     private $game_id;
+    // This player's client id. 
+    private $client_id;
     // Each color's player id.
     private $white_id;
     private $black_id;
@@ -27,20 +28,25 @@ class Game {
     private $turn_num;
     // 2D Array containing black_pieces and white_pieces. Is to be encoded to client as a JSON
     private $board_rep = array(array());
-    // Color-specific array that is ready to be encoded to client as a JSON.
+    
+// Calculateable Values
+    // Color-specific array that is ready to be encoded to client as a JSON. (A subset of $board_rep)
     private $black_pieces;
     private $white_pieces;
+    // Denotes the color of the player whose turn it is.  (Dictated by whether the $turn_num is even or odd)
+    private $cur_color; //either "w" or "b"
     
 // Session_Specific Values
-    private $cur_color; // either "w" or "b"
-    private $my_turn; // either true or false
+    // Denotes the color that is associated with 
+    private $my_color; // either "w" or "b"
+    // Tells whether or not it is this client's turn.
+    private $my_turn = FALSE; // either true or false
     private $potential_moves = array();
     
-/*// Default constructor
-    public function __constructor()
+    // Default constructor
+    function __construct()
     {
-        // Get's the client's id. 
-        $client_id = $_SESSION["player"]->get_Id();
+        $this->client_id = $_SESSION["player"]->get_Id();
         
         $db_host = 'mysql.cs.iastate.edu';
         $db_user = 'u309M13';
@@ -56,13 +62,12 @@ class Game {
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql      = "SELECT * FROM $tbl_name WHERE '$client_id' = white_id OR '$client_id' = black_id";
+        $sql      = "SELECT * FROM $tbl_name WHERE white_id = '$this->client_id' OR black_id = '$this->client_id'";
         $result   = $conn->query($sql);
 
         // If result matched $username and $password, table row must be 1 row
-        if ($result->num_rows == 1)
+        if ($result->num_rows === 1)
         {
-            echo "This player is in a Game." . "<br/>";
             if ($row = $result->fetch_assoc())
             {
                 $this->game_id = $row["game_id"];
@@ -79,40 +84,46 @@ class Game {
  //              $this->black_json = $row["black_json"];
  //              $this->white_json = $row["white_json"];
                 
-                if ($client_id == $row["white_id"])
+                // If turn number is even, then it is black's turn. If it is odd, then white's turn.
+                if ($this->turn_num % 2 === 0)
                 {
-                    $this->cur_color='w';
-                    if ($this->turn_num % 2 === 0)
-                    {
-                        $this->my_turn = FALSE;
-                    }
-                    
+                    $this->cur_color = 'b';
                 }
-                else if ($client_id == $row["black_id"])
-                { 
-                    $this->cur_color='b';
-                    if ($this->turn_num % 2 === 0)
-                    {
-                        $this->my_turn = TRUE;
-                    }
+                else
+                {
+                    $this->cur_color = 'w';
                 }
                 
-                $conn->close;
+                // Set values denoting the client's color, and update that the client's color is in the game.
+                if ($this->client_id == $row["white_id"])
+                {
+                    $this->white_in = TRUE;
+                    $this->my_color = 'w';
+                }
+                else if ($this->client_id == $row["black_id"])
+                { 
+                    $this->black_in = TRUE;
+                    $this->my_color='b';
+                }
+                
+                if ($this->my_color == $this->cur_color) $this->my_turn=TRUE;
+                $conn->close();
             }
         }
         else if ($result->num_rows < 1)
         {
+            $this->game_id = -1;
             echo "Player is not in a game. Re-direct to homepage." . "<br/>";
             $conn->close();
             header("location:player0.php");
         }
         else 
         {
+            $this->game_id = -2;
              $conn->close();
              throw new Exception('Multiple instances of player in GAME table.');
         }
-        $conn->close();
-        
+
         if($this->turn_num === null){
             $this->turn_num = 0;
         }
@@ -122,11 +133,15 @@ class Game {
 //            initNewBoard();
             
         $init_moves = 0;
-        setPotentialMoves();
+        // setPotentialMoves();
     }
 // Game Table Value Getters
-    public function get_Game_Id() {
+    public function get_Id_Game() {
         return $this->game_id;
+    }
+    public function get_Id_Client()
+    {
+        return $this->client_id;
     }
     public function get_Id_White() {
         return $this->white_id;
@@ -140,8 +155,14 @@ class Game {
     public function get_Username_Black() {
         return $this->black_username;
     }
+    public function in_Game_White() {
+        return $this->white_in;
+    }
+    public function in_Game_Black() {
+        return $this->black_in;
+    }
     public function get_Turn_Number() {
-        return $this->turn_number;
+        return $this->turn_num;
     }
     public function get_Board_Representation() {
         return $this->board_rep;   
@@ -179,7 +200,7 @@ class Game {
         $this->black_pieces = $black_json;
     }
     
-// Updaters
+/*// Updaters
     public function update_Game()
     {
         $db_host = 'mysql.cs.iastate.edu';
@@ -215,8 +236,9 @@ class Game {
         
         //Close the database connection
         conn->close();
-    }
-    public function end_Game()
+    }*/
+    
+/*    public function end_Game()
     {
             $db_host = 'mysql.cs.iastate.edu';
             $db_user = 'u309M13';
@@ -242,52 +264,53 @@ class Game {
             
             // Close database connection. 
             $conn->close();        
-    }
-//    public function initNewBoard(){
-//            
-//           $PAWN = 0;
-//           $KNIGHT = 1;
-//           $BISHOP = 2;
-//           $ROOK = 3;
-//           $QUEEN = 4;
-//           $KING = 5;
-//         
-//           $json = ' "black":  [{ "piece": ROOK,  "row": 0, "col": 0, "status": UNCAPTURED },
-//                                { "piece": KNIGHT,"row": 0, "col": 1, "status": UNCAPTURED },
-//                                { "piece": BISHOP,"row": 0, "col": 2, "status": UNCAPTURED },
-//                                { "piece": KING,  "row": 0, "col": 3, "status": UNCAPTURED },
-//                                { "piece": QUEEN, "row": 0, "col": 4, "status": UNCAPTURED },
-//                                { "piece": BISHOP,"row": 0, "col": 5, "status": UNCAPTURED },
-//                                { "piece": KNIGHT,"row": 0, "col": 6, "status": UNCAPTURED },
-//                                { "piece": ROOK,  "row": 0, "col": 7, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 1, "col": 0, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 1, "col": 1, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 1, "col": 2, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 1, "col": 3, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 1, "col": 4, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 1, "col": 5, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 1, "col": 6, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 1, "col": 7, "status": UNCAPTURED }, ],
-//                     "white": [ { "piece": ROOK,  "row": 7, "col": 0, "status": UNCAPTURED },
-//                                { "piece": KNIGHT,"row": 7, "col": 1, "status": UNCAPTURED },
-//                                { "piece": BISHOP,"row": 7, "col": 2, "status": UNCAPTURED },
-//                                { "piece": KING,  "row": 7, "col": 3, "status": UNCAPTURED },
-//                                { "piece": QUEEN, "row": 7, "col": 4, "status": UNCAPTURED },
-//                                { "piece": BISHOP,"row": 7, "col": 5, "status": UNCAPTURED },
-//                                { "piece": KNIGHT,"row": 7, "col": 6, "status": UNCAPTURED },
-//                                { "piece": ROOK,  "row": 7, "col": 7, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 6, "col": 0, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 6, "col": 1, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 6, "col": 2, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 6, "col": 3, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 6, "col": 4, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 6, "col": 5, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 6, "col": 6, "status": UNCAPTURED },
-//                                { "piece": PAWN,  "row": 6, "col": 7, "status": UNCAPTURED }, ]';
-//
-//        $this->board_rep = json_decode($json, true);
-//        var_dump($this->board_rep);
-//    }*/
+    }*/
+    
+/*    public function initNewBoard(){
+            
+           $PAWN = 0;
+           $KNIGHT = 1;
+           $BISHOP = 2;
+           $ROOK = 3;
+           $QUEEN = 4;
+           $KING = 5;
+         
+           $json = ' "black":  [{ "piece": ROOK,  "row": 0, "col": 0, "status": UNCAPTURED },
+                                { "piece": KNIGHT,"row": 0, "col": 1, "status": UNCAPTURED },
+                                { "piece": BISHOP,"row": 0, "col": 2, "status": UNCAPTURED },
+                                { "piece": KING,  "row": 0, "col": 3, "status": UNCAPTURED },
+                                { "piece": QUEEN, "row": 0, "col": 4, "status": UNCAPTURED },
+                                { "piece": BISHOP,"row": 0, "col": 5, "status": UNCAPTURED },
+                                { "piece": KNIGHT,"row": 0, "col": 6, "status": UNCAPTURED },
+                                { "piece": ROOK,  "row": 0, "col": 7, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 1, "col": 0, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 1, "col": 1, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 1, "col": 2, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 1, "col": 3, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 1, "col": 4, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 1, "col": 5, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 1, "col": 6, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 1, "col": 7, "status": UNCAPTURED }, ],
+                     "white": [ { "piece": ROOK,  "row": 7, "col": 0, "status": UNCAPTURED },
+                                { "piece": KNIGHT,"row": 7, "col": 1, "status": UNCAPTURED },
+                                { "piece": BISHOP,"row": 7, "col": 2, "status": UNCAPTURED },
+                                { "piece": KING,  "row": 7, "col": 3, "status": UNCAPTURED },
+                                { "piece": QUEEN, "row": 7, "col": 4, "status": UNCAPTURED },
+                                { "piece": BISHOP,"row": 7, "col": 5, "status": UNCAPTURED },
+                                { "piece": KNIGHT,"row": 7, "col": 6, "status": UNCAPTURED },
+                                { "piece": ROOK,  "row": 7, "col": 7, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 6, "col": 0, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 6, "col": 1, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 6, "col": 2, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 6, "col": 3, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 6, "col": 4, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 6, "col": 5, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 6, "col": 6, "status": UNCAPTURED },
+                                { "piece": PAWN,  "row": 6, "col": 7, "status": UNCAPTURED }, ]';
+
+        $this->board_rep = json_decode($json, true);
+        var_dump($this->board_rep);
+    
+ } */
 }
 ?>
-    </body></html>
