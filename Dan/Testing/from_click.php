8bb3e7d94771f46de0243a39a099a9c9a7d53c50
tjj <tjj@817968c2-256c-45f1-8ff6-e7619683c4bf>
2014-12-12 11:19:03 +0000

    deleted shits and gigs

--- Dan/Testing/from_click.php
+++ Dan/Testing/from_click.php
@@ -13,11 +13,5 @@ require_once "engine.php";
     // call PHP function to produce legal moves for square clicked. 
     // $newArray = getPotentialMoves($r, $c);
     $newArray = array(array('row'=>$r,'col'=>$c));
-    $shits_and_gigs = new Game;
-    if ($shits_and_gigs->is_Clients_Turn())
-    {
-        $shits_and_gigs->increment_Turn_Number();
-        $shits_and_gigs->update_Game();
-    }
     echo json_encode($newArray);
 ?>
